3552142ba56303d51448f4b02abb6dc4e3eb3c18
Tinkerrer <artem.shaykhanov@mail.ru>
2021-12-19 02:12:56 +0300

    deleted trash example.cpp

--- sources/example.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright 2021 Tinkerrer
-
-#include <example.hpp>
-#include <iostream>
-
-auto example() -> void {
-  std::cout << "example";
-  // throw std::runtime_error("not implemented");
-}
